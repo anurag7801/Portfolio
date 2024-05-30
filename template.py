dc57ec22b34d0088c32f742d3d040968936e676a
anurag7801 <anuragk7801@gmail.com>
2024-05-30 19:00:02 +0530

    template added

---
+++ template.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+import logging
+
+project_name = "portfolio"
+
+list_of_files = [
+    f"css/bootstrap/bootstrap-grid.css",
+    f"css/bootstrap/bootstrap-reboot.css",
+    f"css/css/mixins/_text-hide.css",
+    f"css/css/bootstrap-reboot.css",
+    f"css/animate.css",
+    f"css/aos.css",
+    f"css/bootstrap.min.css",
+    f"css/flaticon.css",
+    f"css/icomoon.css",
+    f"css/ionicons.min.css",
+    f"css/magnific-popup.css",
+    f"css/open-iconic-bootstrap.min.css",
+    f"css/owl.carousel.min.css",
+    f"css/owl.theme.default.min.css",
+    f"css/style.css",
+    f"fonts/a.py",
+    f"images/a.py",
+    f"js/a.py",
+    "index.html"
+    
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already exists")
+
